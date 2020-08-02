89f65c2bbdf8e3d8e3cbee9001715bf558be961d
EvanCharles16 <evancharles1609@gmail.com>
2020-08-02 19:59:49 +0700

    vue add router

---
+++ src/components/AppBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-bar">
+    <span class="app-bar-l" @click="$emit('left')">
+      <i :class="['fa', `fa-${left}`]"></i>
+    </span>
+    <h1>{{title}}</h1>
+    <span class="app-bar-r" @click="$emit('right')">
+      <i :class="['fa', `fa-${right}`]"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    left: {
+      type: String,
+      default: "chevron-left",
+    },
+    right: {
+      type: String,
+      default: "ellipsis-v",
+    },
+  },
+};
+</script>
+
+<style>
+</style>
